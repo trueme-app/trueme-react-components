02c0a63303ccbc527a44ebdced1188250caae9da
struct78 <david@struct.com.au>
2019-07-19 08:31:14 +1000

    Add types

--- src/components/button/styles.ts
+++ src/components/button/styles.ts
@@ -1,19 +1,26 @@
 import { css } from 'styled-components'
+import { ThemeProps, IColourDefinition } from '@trueme/design-system/react'
+import { ButtonProps } from './index'
 
-const baseStyles = css<any>`
-  background-color: ${(props) => props.theme.colours[props.variant].base};
+const defaultProps = {
+  variant: 'secondary'
+}
+
+const baseStyles = css<ThemeProps & ButtonProps>`
+  background-color: ${(props) => props.theme.colours[props.variant || defaultProps.variant].base};
   border: 0;
   border-radius: ${(props) => props.theme.borders.radius.base};
   padding: ${(props) => props.theme.spacing.md} ${(props) => props.theme.spacing.xl};
 
   &:hover {
-    background-color: ${(props) => props.theme.colours[props.variant].light};
+    background-color: ${(props) => props.theme.colours[props.variant || defaultProps.variant].light};
   }
 `
 
-const attrStyles = (props: any) => {
+const attrStyles = (props: ThemeProps & ButtonProps) => {
+  const colour: IColourDefinition = props.theme.colours[props.variant || defaultProps.variant] as IColourDefinition
   return {
-    underlayColor: props.theme.colours.secondary.light,
+    underlayColor: colour.light,
   }
 }
 
