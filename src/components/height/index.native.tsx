8bfb92c7b2eae700f8b16a80bca88a26f3ece476
struct78 <david@struct.com.au>
2019-09-09 09:40:34 +1000

    Add height component

---
+++ src/components/height/index.native.tsx
@@ -0,0 +1,92 @@
+import React, { useEffect, useRef, useState } from 'react'
+import Picker from '../picker/index.native'
+
+export interface HeightProps {
+  width: number | string
+  selectedValue: number
+  onValueChange: (value: number) => void
+}
+
+interface Height {
+  label: string
+  value: number
+}
+
+interface State {
+  value: number
+}
+
+const useCompare = (val: any) => {
+  const prevVal = usePrevious(val)
+  return prevVal !== val
+}
+
+const usePrevious = (value: any) => {
+  const ref = useRef()
+  useEffect(() => {
+    ref.current = value
+  })
+  return ref.current
+}
+
+const startingHeight = 55
+const factor = 30.48
+const inch = 2.54
+
+const options: Height[] = Array.from(Array(200).keys())
+  .filter((n: number) => n >= startingHeight)
+  .map((n: number) => {
+    const feetInches = (n / factor)
+    .toFixed(2)
+    .split('')
+    .filter((value: string) => value !== '.')
+
+    const formattedHeight = feetInches.slice(0, 2).join(`'`)
+
+    return {
+      label: `${Math.floor(n % inch) !== 0 ? `${n}cm` : `${n}cm (${formattedHeight})`}`,
+      value: n,
+    }
+  })
+
+const defaultHeight = 178
+
+const HeightPicker = ({
+  onValueChange,
+  selectedValue = defaultHeight,
+}: {
+  onValueChange: (value: number) => void,
+  selectedValue: number,
+}) => {
+  const hasUpdated = useCompare(selectedValue)
+  const [state, setState] = useState<State>({
+    value: 0,
+  })
+
+  useEffect(() => {
+    if (hasUpdated) {
+      setState({
+        value: selectedValue,
+      })
+    }
+  })
+
+  const handleValueChange = (value: number) => {
+    setState({
+      ...state,
+      value,
+    })
+
+    if (onValueChange) {
+      onValueChange(value)
+    }
+  }
+
+  return (
+    <Picker selectedValue={selectedValue} onValueChange={handleValueChange}>
+      {options.map(({ label, value }) => <Picker.Item key={value} label={label} value={value}/>)}
+    </Picker>
+  )
+}
+
+export default HeightPicker
--- src/index.native.ts
+++ src/index.native.ts
@@ -3,6 +3,7 @@ import DateOfBirthPicker from './components/dob/index.native'
 import Checkbox from './components/forms/checkbox/index.native'
 import TextInput from './components/forms/text-input/index.native'
 import Heading from './components/heading/index.native'
+import HeightPicker from './components/height/index.native'
 import Picker from './components/picker/index.native'
 import Dots from './components/slider/dots/index.native' // TODO: Rename this to Slider.Dots
 
@@ -12,6 +13,7 @@ export {
   DateOfBirthPicker,
   Dots,
   Heading,
+  HeightPicker,
   Picker,
   TextInput,
 }
